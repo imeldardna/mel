71b0361b2966be0edf9fc5e25abb342188fc2c7c
delasanti <delasantii1@gmail.com>
2024-02-29 15:26:56 +0700

    first commit

---
+++ app/Models/Product.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'price',
+        'description',
+        'image',
+        'stock'
+    ];
+
+    public function orders()
+    {
+        return $this->belongsToMany(Order::class);
+    }
+    public function carts()
+    {
+        return $this->hasMany(cart::class);
+    }
+
+    public function transaction() 
+    {         
+        return $this->hasMany(Transaction::class);     
+    }     
+   
+    public function cart()    
+    {         
+        return $this->hasMany(Cart::class);   
+    }
+}
+
